25cc4778a0578679b8e8328873d94db456496f1d
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2023-05-30 19:27:12 +0200

    feat: add a base controller class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Exports here are needed for the builder. It compiles them into "dist".
+ * None of these exports are used in imports in applications.
+ */
 export * as services from './services';
 export * as types from './types';
 export * as expressMiddlewares from './express-middlewares';
+export * as Controller from './Controller';
