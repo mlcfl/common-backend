398a27cc63595764a9536392fd1c421c062321d7
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2024-02-13 20:40:28 +0100

    fix(routes): fix double initialization of the default app

--- src/Server/setSubdomainRoutes.ts
+++ src/Server/setSubdomainRoutes.ts
@@ -1,5 +1,5 @@
 import {join} from 'node:path';
-import type {Express} from 'express';
+import type {Express, Request, Response} from 'express';
 import vhost from 'vhost';
 import {Fs, type Project} from '~/services';
 import {global404} from './global404';
@@ -9,13 +9,25 @@ import {global404} from './global404';
  */
 export const setSubdomainsRoutes = async (server: Express, project: Project): Promise<void> => {
 	const {
-		env: {HOST},
+		env: {HOST, SERVER_PORT},
+		config: {defaultApp},
 		flatApps,
 	} = project;
+
+	const redirectHandler = ({protocol}: Request, res: Response) => {
+		const host = project.isLocalhost() ? `${HOST}:${SERVER_PORT}` : HOST;
+		res.redirect(301, `${protocol}://${host}`);
+	};
 
 	for (const app of flatApps) {
 		const entry = Fs.getBackendEntryPoint(app);
 		const exists = await Fs.exists(Fs.rootPath, entry);
+
+		// Redirect the subdomain to the main domain to avoid the default application being initialized twice
+		if (app === defaultApp) {
+			server.use(vhost(`${app}.${HOST}`, redirectHandler));
+			continue;
+		}
 
 		if (!exists) {
 			server.use(vhost(`${app}.${HOST}`, await global404(app)));
